dcac927a6c64c494d0de31bada129cfe29d54a28
AnirudhVats975 <anirudhvats975@gmail.com>
2021-11-28 18:45:05 -0800

    some changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
-$(document).on('ready', function() {
-    $(".variable").slick({
+// slikslider
+$(document).ready(function() {
+    $('.variable').slick({
         autoplay: true,
-        slidesToShow:3,
-        slidesToScroll:1,
-        dots: true,
-        infinite: true,
-        variableWidth: true
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
 
-  
-      })};
+    });
+
+});
+
 
 
 
@@ -19,32 +32,45 @@ const LoginPopup = document.getElementById("LoginPopup");
 const overlayer = document.querySelector(".overlayer");
 
 // open popup
-LoginPopup.addEventListener("click", (e)=>{
+LoginPopup.addEventListener("click", (e) => {
     e.preventDefault();
-    loginPopup.style.display ="block";
+    loginPopup.style.display = "block";
     overlayer.style.opacity = "1";
+    document.querySelector("body").style.overflow = 'hidden';
+    loginPopup.classList.add('show');
 
 })
 
 // closePopup 
-function closePopup(){
-    loginPopup.style.display ="none";
-    overlayer.style.opacity = "0";  
+function closePopup() {
+    loginPopup.style.display = "none";
+    overlayer.style.opacity = "0";
+    document.querySelector("body").style.overflow = 'auto';
+    loginPopup.classList.add('show');
 }
 
 
 // login form popup open after 12 seconde on page reload  
-let showPopup = function () {
-    setTimeout(function(){ 
-        loginPopup.style.display ="block";
-        overlayer.style.opacity = "1";  
-     }, 12000);
+let showPopup = function() {
+    setTimeout(function() {
+        loginPopup.style.display = "block";
+        overlayer.style.opacity = "1";
+        document.querySelector("body").style.overflow = 'hidden';
+    }, 12000);
 };
 showPopup();
 
 
 
 // sidebar menu 
- function sideBar(){
-     console.log("click")
- }
+const sidebarWrapper = document.querySelector(".sidebarWrapper");
+
+function sideBar() {
+    sidebarWrapper.style.display = "block";
+    overlayer.style.opacity = "1";
+}
+
+function closeSideBar() {
+    sidebarWrapper.style.display = "none";
+    overlayer.style.opacity = "0";
+}
